12f38be2d9747364dfe8640b78c62078d0aa11d5
Fosterpaladin3 <juanchopablo753@gmail.com>
2025-11-30 11:55:47 -0500

    arreglo entorno

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\USUARIO\Desktop\Codigo\GGastosmart-JP-Y-D\venv'
+set -gx VIRTUAL_ENV 'C:\Users\USUARIO\Desktop\Codigo\GGastosmart-JP-Y-D1\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
